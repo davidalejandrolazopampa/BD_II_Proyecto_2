f93182ce29e32ecfaa1da393622842be9cc35d5d
alonso804 <alonsobs804@gmail.com>
2020-11-26 18:47:55 -0500

    finish preprocessing

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -1,22 +1,53 @@
 import os
-import re
-import codecs
 import json
-from nltk.stem import SnowballStemmer
-
 import nltk 
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
-import itertools
-import collections
-from collections import Counter
 
-from functions import getStopWords
+stemmer = SnowballStemmer('spanish')
 
-global dataPath = '../../data/'
+def getStopWords(stopListPath = '../files/stopList.txt'):
+    stopFile = open(stopListPath, "r")
+    stopWords = []
 
-def preprocessing():
+    for line in stopFile:
+        stopWords.append(line)
+
+    stopWords += ['(', ')', '=', '+', '-', '|', '#', 'x', '/', '@', '?', '.',',', ';', '«', ':', '¿', '!', '¡', '<', '>', '😆', '👍', '_', '...', '👍🏻', '🏡', '”', '“', 'de', 'es', 'un', 'a']
+
+    stopFile.close()
+
+    return stopWords
+
+def preprocessing(dataPath = '../../data/'):
     dataList = os.listdir(dataPath)
     stopWords = getStopWords()
 
     for dataFile in dataList:
+        data = open(dataPath + dataFile, 'r')
+        tweets = json.load(data)
+
+        result = {}
+
+        for tweet in tweets:
+            if tweet['retweeted'] is False:
+                dataId = tweet['id']
+                text = tweet['text'].lower()
+
+                text = nltk.word_tokenize(text)
+                cleanText = []
+
+                for word in text:
+                    if word not in stopWords:
+                        temp = stemmer.stem(word)
+                        cleanText.append(temp)
+
+                result[dataId] = cleanText
+                # print(dataId, result[dataId])
+
+    dataList.close()
+                
+    return result
+
+
+print(preprocessing())
